f515d32057b451e7bf0e1285104ffb6b523d188b
AdriQD <adrianomacaronep@gmail.com>
2021-04-01 23:18:45 +0200

    added function for generating random errors in the measurement outcomes, using a multi modal normal distribution

--- quantum_prediction/utils/utils.py
+++ quantum_prediction/utils/utils.py
@@ -5,3 +5,13 @@ def load_dataset(path):
         data_dict = pickle.load(filino)
     return data_dict
 
+def jumps(state1,state2,cov):
+    errors = hstack((multivariate_normal(state1,cov,1)/4., multivariate_normal(state2,cov,1)/4.))[0]
+    noised = hstack((state1,state2)) + errors
+     
+    norm1 = array([np.sum(noised[:4])]*4)
+    norm2 = array([np.sum(noised[4:])]*4)
+    final = noised/hstack( (norm1,norm2))
+    return final
+
+
